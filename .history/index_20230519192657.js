17d9f513d1736757a1917703880ad078ae821cfb
React1 <React1@flyweis.technology>
2023-05-19 19:28:07 +0530

    chnages

--- .history/index_20230519192657.js
+++ .history/index_20230519192657.js
@@ -1,20 +1,32 @@
-/** @format */
-
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const http = require('http');
 const bodyparser = require("body-parser");
+
 const serverless = require("serverless-http");
-const nodemailer = require("./src/route/route");
+
+const nodemailer = require("./src/route/route")
+
+
+
 require("dotenv").config();
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-const PORT = 1200;
-app.get("/home", (req, res) => {
-  res.status(200).send({ msg: "Working App" });
+
+const PORT =  2006;
+
+
+app.get("/home",(req, res) => {
+  res.status(200).send({msg:"Working App"});
 });
-app.use("/api/v1/", nodemailer);
+
+ app.use("/api/v1/", nodemailer)
+
+
 app.listen(PORT, () => {
   console.log(`listening on port `);
 });
@@ -22,3 +34,4 @@ app.listen(PORT, () => {
 module.exports = {
   handler: serverless(app),
 };
+
--- .history/index_20230519192700.js
+++ .history/index_20230519192700.js
@@ -1,20 +1,31 @@
-/** @format */
-
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const bodyparser = require("body-parser");
+
 const serverless = require("serverless-http");
-const nodemailer = require("./src/route/route");
+
+const nodemailer = require("./src/route/route")
+
+
+
 require("dotenv").config();
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-const PORT = 1200;
-app.get("/home", (req, res) => {
-  res.status(200).send({ msg: "Working App" });
+
+const PORT =  2006;
+
+
+app.get("/home",(req, res) => {
+  res.status(200).send({msg:"Working App"});
 });
-app.use("/api/v1/", nodemailer);
+
+ app.use("/api/v1/", nodemailer)
+
+
 app.listen(PORT, () => {
   console.log(`listening on port `);
 });
@@ -22,3 +33,4 @@ app.listen(PORT, () => {
 module.exports = {
   handler: serverless(app),
 };
+
