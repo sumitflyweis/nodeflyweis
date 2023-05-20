5763c9ce193a45a7fbc670a5ed534b4f87b3f0f3
sumitflyweis <node3@flyweis.technology>
2023-05-20 15:00:25 +0530

    a

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -15,12 +15,26 @@ module.exports.addContactData = async (req, res) => {
         // secure: true,
         secure : false,
         auth: {
-          // user: "info@flyweis.technology",
-          // pass: "ygkojtgemllsgpgs",
-          user: "node2@flyweis.technology",
-          pass: "ayesha@9818#",
+          user: "info@flyweis.technology",
+          pass: "ygkojtgemllsgpgs",
+          // user: "node2@flyweis.technology",
+          // pass: "ayesha@9818#",
         },
       });
+
+
+    //   await new Promise((resolve, reject) => {
+    //     // verify connection configuration
+    //     transporter.verify(function (error, success) {
+    //         if (error) {
+    //             console.log(error);
+    //             reject(error);
+    //         } else {
+    //             console.log("Server is ready to take our messages");
+    //             resolve(success);
+    //         }
+    //     });
+    // });
 
     var mail = {
       from: "info@flyweis.technology",
@@ -30,6 +44,36 @@ module.exports.addContactData = async (req, res) => {
       text: `Name: ${name}, Phone: ${phone}, Subject: ${subject} , Message : ${message}`,
     };
 
+
+  //   const mailData = {
+  //     from: {
+  //         //name: `${firstName} ${lastName}`,
+  //         address: "info@flyweis.technology",
+  //     },
+  //     replyTo: "node3@flyweis.technology",
+  //     to: "node3@flyweis.technology",
+  //     subject: `form message`,
+  //     text: message,
+  //     html: `${message}`,
+  // };
+
+
+//   await new Promise((resolve, reject) => {
+//     // send mail
+//     transporter.sendMail(mailData, (err, info) => {
+//         if (err) {
+//             console.error(err);
+//             reject(err);
+//         } else {
+//             console.log(info);
+//             resolve(info);
+//         }
+//     });
+// });
+
+// res.status(200).json({ status: "OK" });
+// }
+    
     transporter.sendMail(mail, function (error, info) {
       if (error) {
         console.log(error);
@@ -37,6 +81,9 @@ module.exports.addContactData = async (req, res) => {
         console.log("Email sent: " + info.response);
       }
     });
+
+
+
 
     // let xhr = new XMLHttpRequest();
     // xhr.open("post", "/addContactData");
@@ -46,7 +93,69 @@ module.exports.addContactData = async (req, res) => {
     // };
 
     return res.status(200).json({ msg: "Message send successfully" });
-  } catch (error) {
+  }
+   catch (error) {
+    console.log(error)
     return res.status(500).json({ errors: error });
   }
 };
+
+
+
+
+// const nodemailer = require("nodemailer");
+
+// export default async (req, res) => {
+
+// const { firstName, lastName, email, message } = JSON.parse(req.body);
+
+// const transporter = nodemailer.createTransport({
+//     port: 465,
+//     host: "smtp.gmail.com",
+//     auth: {
+//         user: "myEmail@gmail.com",
+//         pass: "password",
+//     },
+//     secure: true,
+// });
+
+// await new Promise((resolve, reject) => {
+//     // verify connection configuration
+//     transporter.verify(function (error, success) {
+//         if (error) {
+//             console.log(error);
+//             reject(error);
+//         } else {
+//             console.log("Server is ready to take our messages");
+//             resolve(success);
+//         }
+//     });
+// });
+
+// const mailData = {
+//     from: {
+//         name: `${firstName} ${lastName}`,
+//         address: "myEmail@gmail.com",
+//     },
+//     replyTo: email,
+//     to: "recipient@gmail.com",
+//     subject: `form message`,
+//     text: message,
+//     html: `${message}`,
+// };
+
+// await new Promise((resolve, reject) => {
+//     // send mail
+//     transporter.sendMail(mailData, (err, info) => {
+//         if (err) {
+//             console.error(err);
+//             reject(err);
+//         } else {
+//             console.log(info);
+//             resolve(info);
+//         }
+//     });
+// });
+
+// res.status(200).json({ status: "OK" });
+// };
