d08b061b33fea6d896d797ca39b50d8b3106acd3
React1 <React1@flyweis.technology>
2023-05-20 12:15:13 +0530

    Info

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,44 +1,11 @@
+/** @format */
+
 const payment = require("../model/model");
 const nodemailer = require("nodemailer");
 var XMLHttpRequest = require("xhr2");
 
-
-
-// // create reusable transporter object using the default SMTP transport
-// let transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 587,
-//     secure: false,
-//     auth: {
-//       user: "node2@flyweis.technology",
-//       pass: "ayesha@9818#",
-//     }
-// });
-
-// // setup email data with unicode symbols
-// let mailOptions = {
-//     from: 'node2@flyweis.technology', // sender address
-//     to: 'node3@flyweis.technology', // list of receivers
-//     subject: 'payment', // Subject line
-//     text: 'payment done9', // plain text body
-//     html: '<b></b>' // html body
-// };
-
-// // send mail with defined transport object
-// transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log('Email sent: ' + info.response);
-//     }
-// });
-
-
-
-
-
 module.exports.addContactData = async (req, res) => {
-  const { name, email, phone, subject , message } = req.body;
+  const { name, email, phone, subject, message } = req.body;
   try {
     const transporter = nodemailer.createTransport({
       service: "Gmail",
