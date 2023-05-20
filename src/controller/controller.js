998a45687dc6d6946c8d5e95bca22445f5e314fb
sumitflyweis <node3@flyweis.technology>
2023-05-20 15:05:57 +0530

    ac

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -23,64 +23,64 @@ module.exports.addContactData = async (req, res) => {
       });
 
 
-    //   await new Promise((resolve, reject) => {
-    //     // verify connection configuration
-    //     transporter.verify(function (error, success) {
-    //         if (error) {
-    //             console.log(error);
-    //             reject(error);
-    //         } else {
-    //             console.log("Server is ready to take our messages");
-    //             resolve(success);
-    //         }
-    //     });
-    // });
-
-    var mail = {
-      from: "info@flyweis.technology",
-      // to: "info@flyweis.technology",
-      to: "node3@flyweis.technology",
-      subject: `${email} want to contact you`,
-      text: `Name: ${name}, Phone: ${phone}, Subject: ${subject} , Message : ${message}`,
-    };
-
-
-  //   const mailData = {
-  //     from: {
-  //         //name: `${firstName} ${lastName}`,
-  //         address: "info@flyweis.technology",
-  //     },
-  //     replyTo: "node3@flyweis.technology",
-  //     to: "node3@flyweis.technology",
-  //     subject: `form message`,
-  //     text: message,
-  //     html: `${message}`,
-  // };
-
-
-//   await new Promise((resolve, reject) => {
-//     // send mail
-//     transporter.sendMail(mailData, (err, info) => {
-//         if (err) {
-//             console.error(err);
-//             reject(err);
-//         } else {
-//             console.log(info);
-//             resolve(info);
-//         }
-//     });
-// });
-
-// res.status(200).json({ status: "OK" });
-// }
-    
-    transporter.sendMail(mail, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
+      await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
     });
+
+    // var mail = {
+    //   from: "info@flyweis.technology",
+    //   // to: "info@flyweis.technology",
+    //   to: "node3@flyweis.technology",
+    //   subject: `${email} want to contact you`,
+    //   text: `Name: ${name}, Phone: ${phone}, Subject: ${subject} , Message : ${message}`,
+    // };
+
+
+    const mailData = {
+      from: {
+          //name: `${firstName} ${lastName}`,
+          address: "info@flyweis.technology",
+      },
+      replyTo: "node3@flyweis.technology",
+      to: "node3@flyweis.technology",
+      subject: `form message`,
+      text: message,
+      html: `${message}`,
+  };
+
+
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+});
+
+res.status(200).json({ status: "OK" });
+}
+    
+    // transporter.sendMail(mail, function (error, info) {
+    //   if (error) {
+    //     console.log(error);
+    //   } else {
+    //     console.log("Email sent: " + info.response);
+    //   }
+    // });
 
 
 
@@ -92,8 +92,8 @@ module.exports.addContactData = async (req, res) => {
     //   console.log(xhr.responseText);
     // };
 
-    return res.status(200).json({ msg: "Message send successfully" });
-  }
+  //   return res.status(200).json({ msg: "Message send successfully" });
+  // }
    catch (error) {
     console.log(error)
     return res.status(500).json({ errors: error });
